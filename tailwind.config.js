708a633decb327babe5cb9f80e461532d9a9b4d7
Anand Ganesan <76727898+anandcs20@users.noreply.github.com>
2025-11-22 19:07:08 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+          spacing: {
+        header: "80px",
+      },
         },
     },
 
